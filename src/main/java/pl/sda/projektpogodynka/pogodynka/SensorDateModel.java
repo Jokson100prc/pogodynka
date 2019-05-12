7815619283df39d252889ca828013b8fd6cd30e7
unknown <nowacki5@onet.eu>
2019-05-12 10:46:39 +0200

    dodanie zmian

--- src/main/java/pl/sda/projektpogodynka/pogodynka/SensorDateModel.java
+++ src/main/java/pl/sda/projektpogodynka/pogodynka/SensorDateModel.java
@@ -1,4 +1,11 @@
 package pl.sda.projektpogodynka.pogodynka;
 
-public class SensorDateModel {
-}
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+    class SensorDataModel {
+        String sensorId;
+        HashMap<String, Double> values;
+        LocalDateTime date;
+    }
+
